0569f8c6bec834574f8fd8d2f01fe39d1be5a84c
Anushek Prasal <anushekprasal@gmail.com>
2019-06-06 14:18:24 +0530

    Add toggle to disable charging animation [1/2]
    @neobuddy89: Use same toggle for wireless charging animation too.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: ryzenforce990 <ryzenforce990@users.noreply.github.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6737,6 +6737,12 @@ public final class Settings {
         public static final String ARCANE_IDLE_MANAGER = "arcane_idle_manager";
 
         /**
+         * Whether to show charging animation
+         * @hide
+         */
+        public static final String CHARGING_ANIMATION = "charging_animation";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
