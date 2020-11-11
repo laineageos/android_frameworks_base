290bca67cc103a5e693252d3ee625a3fd5ab7400
maxwen <max.weninger@gmail.com>
2020-11-11 16:04:30 +0100

    Allow changing back gesture height [1/2]
    * @neobuddy89: Simplify and cleanup!
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6596,6 +6596,9 @@ public final class Settings {
          */
         public static final String SHOW_APP_VOLUME = "show_app_volume";
 
+        /** @hide */
+        public static final String BACK_GESTURE_HEIGHT = "back_gesture_height";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
