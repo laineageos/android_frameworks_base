4572af3e0a85e384724ee6f1647eb2732cfea346
someone5678 <someone5678@users.noreply.github.com>
2024-09-14 15:24:13 +0530

    base: Move window ignore secure to global settings
    Signed-off-by: ryzenforce990 <ryzenforce990@users.noreply.github.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13029,13 +13029,6 @@ public final class Settings {
         public static final String STYLUS_POINTER_ICON_ENABLED = "stylus_pointer_icon_enabled";
 
         /**
-         * Control whether FLAG_SECURE is ignored for all windows.
-         * @hide
-         */
-        @Readable
-        public static final String WINDOW_IGNORE_SECURE = "window_ignore_secure";
-
-        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
@@ -19600,6 +19593,14 @@ public final class Settings {
          * @hide
          */
         public static final String PM_DOWNGRADE_ALLOWED = "pm_downgrade_allowed";
+
+        /**
+         * Control whether FLAG_SECURE is ignored for all windows.
+         * @hide
+         */
+        @Readable
+        public static final String WINDOW_IGNORE_SECURE = "window_ignore_secure";
+
 
         /**
          * Setting indicating whether Low Power Standby is enabled, if supported.
