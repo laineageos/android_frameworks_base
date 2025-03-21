8b8e4f87682e447589a2867eff2afeae482be15f
Sai Chothave <sai.chothave@oppo.com>
2025-03-21 17:07:22 +0530

    Fix splash screen retention issue during transferStartingWindow
    Problem: {
        1. Splash screen remains attached to animation leash, and not get reparented to targetAR before the removal of StartingWindow.
        2. Due to active anim leash it missed to reparent to targetAR, causes black screen flashes, more frequent on low-end devices.
        3. Steps: Open GMeet, log in, launch app in cold start, observe black screen flash.
    }
    
    Solution: {
        1. Track splash screen state when splash screen is copied `setCopySplashScreenFinish(true/false)`.
        2. Reparent starting window manually to target surface if it has been missed to reparent due to active leash on surfaceAnimator.
    }
    
    Bug: 402600443
    
    https://partnerissuetracker.corp.google.com/u/0/issues/402600443
    Change-Id: Idb67bbdbab63e358d14ba008bd259089e9fabffb
    Signed-off-by: Sai Chothave <sai.chothave@oppo.com>
    Signed-off-by: ryzenforce990 <ryzenforce990@users.noreply.github.com>

--- services/core/java/com/android/server/wm/WindowState.java
+++ services/core/java/com/android/server/wm/WindowState.java
@@ -6245,4 +6245,15 @@ class WindowState extends WindowContainer<WindowState> implements WindowManagerP
     boolean isInsetsAnimationRunning() {
         return mInsetsAnimationRunning;
     }
+
+    private boolean mCopySplashScreenFinish;
+
+    public void setCopySplashScreenFinish(boolean finish) {
+        mCopySplashScreenFinish = finish;
+    }
+
+    public boolean isCopySplashScreenFinish() {
+        return mCopySplashScreenFinish;
+    }
+
 }
